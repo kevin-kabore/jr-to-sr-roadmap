057914f20367f555714b73718cf48ae9ea10d083
Kevin Kabore <kevinkabore@Kevins-iMac.local>
2018-07-03 17:04:46 -0700

    Adds tachyon plus creates Hello component

--- react_redux_module_bundling/robo_homies/src/index.js
+++ react_redux_module_bundling/robo_homies/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
 registerServiceWorker();
