0e8dd4441be3610f85432d51b788d4972557ba31
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-07-06 17:15:09 -0700

    adds ajax call in componentdidmmount

--- react_redux_module_bundling/robo_homies/src/App.js
+++ react_redux_module_bundling/robo_homies/src/App.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
-import { robots } from './robots';
+// import { robots } from './robots';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     };
+    console.log('constructor');
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        return res.json();
+      })
+      .then(users => {
+        this.setState({ robots: users });
+      });
+
+    console.log('componentDidMount');
   }
   onSearchChange = event => {
     this.setState({ searchfield: event.target.value });
@@ -22,8 +34,8 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    console.log(filteredRobots);
-
+    console.log(`Search Results: `, filteredRobots);
+    console.log('render');
     return (
       <div className="tc">
         <h1 className="f1">Robo Homies </h1>
