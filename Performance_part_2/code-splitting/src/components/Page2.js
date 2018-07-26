d811408435c310f1b4209e72d1d75e0500e37a3c
Kevin Kabore <kevinkabore@Kevins-iMac.local>
2018-07-26 12:01:47 -0700

    Adds styling to discern components

--- Performance_part_2/code-splitting/src/components/Page2.js
+++ Performance_part_2/code-splitting/src/components/Page2.js
@@ -8,8 +8,9 @@ const Page2 = ({onRouteChange}) => {
 	          <img src={logo} className="App-logo" alt="logo" />
 	          <h1 className="App-title">Welcome to React</h1>
 	        </header>
-	        <button onClick={() => onRouteChange('page3')}>Page3</button>
 	        <button onClick={() => onRouteChange('page1')}>Page1</button>
+	        <button className="disabled">Page3</button>
+	        <button onClick={() => onRouteChange('page3')}>Page3</button>
       	</div>
 	)
 }
