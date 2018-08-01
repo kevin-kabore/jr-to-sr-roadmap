6f841699b69939b61e5feddc76ca78188606b509
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-07-31 17:04:04 -0700

    completes tests and adds describe block

--- Testing/test/script.test.js
+++ Testing/test/script.test.js
@@ -5,7 +5,18 @@ it('should execute this test', () => {
   expect(1).toEqual(1);
 });
 
-it('is searching with the googleSearch function', () => {
-  expect(googleSearch('test', dbMock)).toEqual([]);
-  expect(googleSearch('cat', dbMock)).toEqual(['cat.com', 'catlovers.com']);
+describe('googleSearch', () => {
+  it('is searching with the googleSearch function', () => {
+    expect(googleSearch('test', dbMock)).toEqual([]);
+    expect(googleSearch('cat', dbMock)).toEqual(['cat.com', 'catlovers.com']);
+  });
+
+  it('should work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('should not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  });
 });
