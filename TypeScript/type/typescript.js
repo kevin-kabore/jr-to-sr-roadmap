3b395b36f4d5cdead26e3e4a0295e59c51986f3f
kevinkabore <kevin@tvgla.com>
2018-10-01 18:26:48 -0700

    Inits typscript and adds notes on new types

--- TypeScript/type/typescript.js
+++ TypeScript/type/typescript.js
@@ -1,4 +1,53 @@
-var sum = function (a, b) {
+"use strict";
+const sum = (a, b) => {
     return a + b;
 };
-var answer = sum(10, 5);
+const answer = sum(10, 5);
+// Bool
+let isCool = true;
+// Number
+let age = 23;
+// String 
+let eyeColor = 'brown';
+let favoriteQuote = `I'm not old, I'm only ${age}`;
+// Array
+let pets = ['cat', 'dog', 'pig'], pets2 = ['lion', 'dragon', 'lizard'];
+// Object 
+let person = {
+    a: 'Kevin'
+};
+// null and undefined
+let und = undefined;
+let nullll = null;
+// Tuple
+// Specify index types
+let basket;
+basket = ['basketball', 5];
+// Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+let sizeName = Size[2];
+let sizeNumber = Size.Small;
+console.log(sizeName); // will output: Medium
+console.log(sizeNumber); // will output: 1
+// Any - Not recommended;
+let whateverType = `DON'T DO THIS!!!`;
+whateverType = basket;
+// Void 
+// function returns nothing
+let sing = () => {
+    console.log('Returns nothing!');
+};
+// Never
+// Function never returns
+// Doesn't have an endpoint. Usually throws an error
+let error = () => {
+    throw Error('oops');
+};
+// Interface
+// Used well with objects
--- TypeScript/type/typescript.ts
+++ TypeScript/type/typescript.ts
@@ -3,3 +3,60 @@ const sum = (a: number, b: number): number => {
 }
 
 const answer = sum(10, 5);
+
+// Bool
+let isCool: boolean = true;
+
+// Number
+let age: number = 23;
+
+// String 
+let eyeColor: string = 'brown';
+let favoriteQuote: string = `I'm not old, I'm only ${age}`;
+
+// Array
+let pets: string[] = ['cat', 'dog', 'pig'],
+	pets2: Array<string> = ['lion', 'dragon', 'lizard'];
+
+
+// Object 
+let person: object = {
+	a: 'Kevin'
+}
+
+// null and undefined
+let und: undefined = undefined;
+let nullll: null = null;
+
+// Tuple
+// Specify index types
+let basket: [string, number];
+basket = ['basketball', 5];
+
+// Enum
+enum Size { Small = 1, Medium= 2, Large= 3};
+let sizeName: string = Size[2];
+let sizeNumber: number = Size.Small;
+console.log(sizeName); // will output: Medium
+console.log(sizeNumber); // will output: 1
+
+// Any - Not recommended;
+let whateverType: any = `DON'T DO THIS!!!`;
+whateverType = basket;
+
+
+// Void 
+// function returns nothing
+let sing = (): void => {
+	console.log('Returns nothing!');
+}
+
+// Never
+// Function never returns
+// Doesn't have an endpoint. Usually throws an error
+let error = () => {
+	throw Error('oops');
+}
+
+// Interface
+// Used well with objects
